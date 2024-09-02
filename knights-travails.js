2204dbe67854d74ba0efb8a17eb8111a393bf011
Alex Seeley <alexseeley134@gmail.com>
2024-09-01 19:58:02 -0600

    Set up repository for knights travails project

---
+++ knights-travails.js
@@ -0,0 +1,3 @@
+// The goal is to build a function `knightMoves` that shows the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
+
+console.log("Hello, knights travails!");
